4cf4db52281f96be79f751df2e897f3832223b09
Sagar Gupta <sgrkmr12@gmail.com>
2017-04-14 19:59:24 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/complaints/jd/jantadurbar/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.complaints.jd.jantadurbar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreenActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+                if(sharedPreferences.getBoolean("IsAdmin",false)){
+                    startActivity(new Intent(SplashScreenActivity.this, AdminActivity.class));
+                }
+                else {
+                    Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
